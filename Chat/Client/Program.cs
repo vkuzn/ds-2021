8a8b72ff8c13708a50c911e41dfec89e9cf242d6
Vasily Kuznetsov <vasily.kuznetsov@travelline.ru>
2021-04-08 14:33:33 +0300

    Add PA6 description

---
+++ Chat/Client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        public static void StartClient()
+        {
+            try
+            {
+                // Разрешение сетевых имён
+                IPAddress ipAddress = IPAddress.Loopback;
+                //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                //IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // CREATE
+                Socket sender = new Socket(
+                    ipAddress.AddressFamily,
+                    SocketType.Stream, 
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    // CONNECT
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Удалённый адрес подключения сокета: {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Подготовка данных к отправке
+                    byte[] msg = Encoding.UTF8.GetBytes("Hello, world!<EOF>");
+
+                    // SEND
+                    int bytesSent = sender.Send(msg);
+
+                    // RECEIVE
+                    byte[] buf = new byte[1024];
+                    int bytesRec = sender.Receive(buf);
+
+                    Console.WriteLine("Ответ: {0}",
+                        Encoding.UTF8.GetString(buf, 0, bytesRec));
+
+                    // RELEASE
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+    }
+}
---
+++ Chat/Server/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        public static void StartListening()
+        {
+
+            // Разрешение сетевых имён
+            
+            // Привязываем сокет ко всем интерфейсам на текущей машинe
+            IPAddress ipAddress = IPAddress.Any; 
+            
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // CREATE
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                // BIND
+                listener.Bind(localEndPoint);
+
+                // LISTEN
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание соединения клиента...");
+                    // ACCEPT
+                    Socket handler = listener.Accept();
+
+                    Console.WriteLine("Получение данных...");
+                    byte[] buf = new byte[1024];
+                    string data = null;
+                    while (true)
+                    {
+                        // RECEIVE
+                        int bytesRec = handler.Receive(buf);
+
+                        data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Полученный текст: {0}", data);
+
+                    // Отправляем текст обратно клиенту
+                    byte[] msg = Encoding.UTF8.GetBytes(data);
+
+                    // SEND
+                    handler.Send(msg);
+
+                    // RELEASE
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск сервера...");
+            StartListening();
+
+            Console.WriteLine("\nНажмите ENTER чтобы выйти...");
+            Console.Read();
+        }
+    }
+}
